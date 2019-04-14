a061367c19d1229200e56b1d2893375f6f4ebea3
waltson <waltson.silva@hotmail.com>
2019-04-14 11:01:49 -0300

    Commit inicial do prototipo de microservice

---
+++ microservicePrototipo/src/main/java/com/microservice/prototipo/MicroservicePrototipoApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.prototipo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicePrototipoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicePrototipoApplication.class, args);
+	}
+
+}
